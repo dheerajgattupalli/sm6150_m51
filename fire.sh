0951dd6b9caa474944f4ad011bcaba61cdb6b735
firemax13 <mydadisbrave2@gmail.com>
2020-09-26 05:22:17 +0000

    build-script: improved it & align it with
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- fire.sh
+++ fire.sh
@@ -4,10 +4,11 @@ set -e
 
 # Put the Kernel Path in here and fire it up
 # No need to change the whole and entire file to set-up kernel path
-KERNEL_PATH=/home/firemax13/m51kernel
+KERNEL_PATH=/home/Vevelo/firemax13/m51kernel
+TOOLCHAIN_PATH=/home/Vevelo/firemax13
 
 # Export GCC and ARCH
-export CROSS_COMPILE=$KERNEL_PATH/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=$TOOLCHAIN_PATH/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64
 
 # CleanUP Output Folder
@@ -19,12 +20,12 @@ if [ ! -d out ]; then
 fi
 
 # Types, paths, and more etc.
-BUILD_CROSS_COMPILE=$KERNEL_PATH/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$KERNEL_PATH/toolchain/clang/bin/clang
+BUILD_CROSS_COMPILE=$TOOLCHAIN_PATH/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+KERNEL_LLVM_BIN=$TOOLCHAIN_PATH/clang-r353983c/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -C $KERNEL_PATH O=$KERNEL_PATH/out ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE r1q_eur_open_defconfig
+make -C $KERNEL_PATH O=$KERNEL_PATH/out ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE m51_eur_open_defconfig
 make -j8 -C $KERNEL_PATH O=$KERNEL_PATH/out ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
 
 tools/mkdtimg create out/arch/arm64/boot/dtbo.img --page_size=4096 $(find out -name "*.dtbo")
